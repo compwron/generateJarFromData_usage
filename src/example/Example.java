88d54120d8bd5630daf25805319552d37c0beef6
Linda Goldstein <lgoldste@thoughtworks.com>
2014-03-12 15:41:22 -0700

    working usage of jar

--- src/example/Example.java
+++ src/example/Example.java
@@ -1,7 +1,10 @@
 package example;
 
+import presenter.Hierarchy;
+
 public class Example {
     public static void main(String... args){
-        Hierarchy hierarchy = new Hierarchy("");
+        Hierarchy hierarchy = new Hierarchy();
+        System.out.println("Cls list: " + hierarchy.getItemHierarchy().getDivisions().get(0).getDepartments().get(0).getClsList());
     }
 }
